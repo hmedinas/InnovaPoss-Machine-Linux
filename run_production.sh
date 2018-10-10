5c426328fb96963c0e180341d1d0c76228486a7e
Hugo Medina <alexei38@gmail.com>
2018-10-10 12:00:29 +0200

    demos

--- run_production.sh
+++ run_production.sh
@@ -1,5 +1,8 @@
+sleep 50;
 git pull
+sleep 30;
 killall python3
+sleep 10;
 cd  /home/pi/InnovaPoss-Machine-Linux/
 export PYTHONPATH="$PYTHONPATH:/home/pi/InnovaPoss-Machine-Linux/src/"
 python3 ./src/innovapos/worker/app_worker.py ./configs/innovapos_worker+production.config
